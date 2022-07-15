4ef8046a0a92734366f76cd4211ef306bc2e3356
Carlos Marques <catralitos11@gmail.com>
2022-07-15 23:00:13 +0100

    Fiz classe para buffs

--- Assets/Scripts/Player/PlayerBuffs.cs
+++ Assets/Scripts/Player/PlayerBuffs.cs
@@ -5,21 +5,50 @@ namespace Player
 {
     public class PlayerBuffs : MonoBehaviour
     {
-        public int bulletsMultipliersLeft;
-        public int knockbackBulletsLeft;
-        public int piercingBulletsLeft;
-        public int superBulletsLeft;
+        [HideInInspector] public int bulletsMultipliersLeft;
+        [HideInInspector] public int knockbackBulletsLeft;
+        [HideInInspector] public int piercingBulletsLeft;
+        [HideInInspector] public int superBulletsLeft;
+        [HideInInspector] public float speedBuffTimeLeft;
 
-        public float speedBuffTimeLeft;
+        [Header("Buff Values")] public int bulletsMultipliersPerDice;
+        public int knockbackBulletsPerDice;
+        public int piercingBulletsPerDice;
+        public int superBulletsPerDice;
+        public float fireRateDecrease;
+        public float speedBuffTimePerFace;
 
+        private PlayerShooting _shooting = PlayerEntity.Instance.shooting;
+        
         private void Update()
         {
             speedBuffTimeLeft = Mathf.Clamp(speedBuffTimeLeft - Time.deltaTime, 0, Single.PositiveInfinity);
         }
 
-        public void AddToSpeedBuffTime(float time)
+        public void ApplyBuff(int face)
         {
-            speedBuffTimeLeft += time;
+            switch (face)
+            {
+                case 1:
+                    speedBuffTimeLeft += speedBuffTimePerFace;
+                    break;
+                case 2:
+                    //TODO rever se isto está bem
+                    _shooting.fireRate -= fireRateDecrease;
+                    break;
+                case 3:
+                    superBulletsLeft += superBulletsPerDice;
+                    break;
+                case 4:
+                    bulletsMultipliersLeft += bulletsMultipliersPerDice;
+                    break;
+                case 5:
+                    knockbackBulletsLeft += knockbackBulletsPerDice;
+                    break;
+                case 6:
+                    piercingBulletsLeft += piercingBulletsPerDice;
+                    break;
+            }
         }
     }
 }
