7134bbe8aef4bd95f389ed537331c084c7d190f9
Carlos Marques <catralitos11@gmail.com>
2022-07-16 15:42:40 +0100

    Fiz uma curva de progressão na percentagem que muda cada lado por LVLUP

--- Assets/Scripts/Player/PlayerProgression.cs
+++ Assets/Scripts/Player/PlayerProgression.cs
@@ -5,18 +5,22 @@ namespace Player
     public class PlayerProgression : MonoBehaviour
     {
         public float levelGrowthFactor = 2;
+        public float percentageGrowthFactor = 5;
+        public float basePercentageChange = 0.05f;
         public int baseLevelExperience = 100;
         [HideInInspector] public int currentBaseLevelExperience;
         [HideInInspector] public int currentExperience;
         [HideInInspector] public int currentLevel;
         [HideInInspector] public int experienceForNextLevel;
         [HideInInspector] public float nextPercentageIncrease;
+
         private void Start()
         {
             currentLevel = 1;
             currentBaseLevelExperience = 0;
             currentExperience = 0;
             experienceForNextLevel = baseLevelExperience;
+            SetNewPercentageIncrease();
         }
 
         public void AddExperience(int expPoints)
@@ -39,7 +43,8 @@ namespace Player
 
         public void SetNewPercentageIncrease()
         {
-            //TODO meter quanta percentagem mexes com nivel
+            nextPercentageIncrease =
+                Mathf.Round(Mathf.Pow(currentLevel, 1f / percentageGrowthFactor) * basePercentageChange * 100f) / 100f;
         }
     }
 }
