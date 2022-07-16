8d8192885d22c85f3a401a4bee9906879f7ca04d
Carlos Marques <catralitos11@gmail.com>
2022-07-16 10:56:17 +0100

    Renamed bullet script and deleted unnecessary bullet code

--- Assets/Scripts/Bullets/IPooledObject.cs
+++
@@ -1,9 +0,0 @@
-namespace Bullets
-{
-    public interface IPooledObject
-    {
-        void OnObjectSpawn();
-        void OnObjectSpawn(float angle);
-        void OnObjectSpawn(float angle, float maxAngleStep);
-    }
-}
