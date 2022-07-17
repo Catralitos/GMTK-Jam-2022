352e76dd8bda1b9dddb6d448d196cec8eea8ca3b
Diogo Mendonça <diogo.cunhamen@gmail.com>
2022-07-17 15:01:07 +0100

    Added Resolutions

--- Assets/Scripts/UI/OptionsManager.cs
+++ Assets/Scripts/UI/OptionsManager.cs
@@ -1,11 +1,32 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class OptionsManager : MonoBehaviour
 {
+    Resolution[] resolutions;
+
+    public TMPro.TMP_Dropdown resolutionDropdown;
     public Toggle fullscreenToggle;
     private void Start() {
         bool value = GameManager.Instance.fullscreen;
         fullscreenToggle.isOn = value;
+
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for(int i=0; i <resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 }
